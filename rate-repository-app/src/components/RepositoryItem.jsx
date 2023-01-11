9b0ae5b87a6d6220753b14a4f57987e8793d33b9
Markus Holopainen <markusp_holopainen@yahoo.com>
2023-01-11 13:22:19 +0200

    10.3

---
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,29 @@
+import { View, Text } from 'react-native'
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+    </View>
+  )
+}
+
+/*
+ id: 'django.django',
+    fullName: 'django/django',
+    description: 'The Web framework for perfectionists with deadlines.',
+    language: 'Python',
+    forksCount: 21015,
+    stargazersCount: 48496,
+    ratingAverage: 73,
+    reviewCount: 5,
+    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4',
+*/
+
+export default RepositoryItem
