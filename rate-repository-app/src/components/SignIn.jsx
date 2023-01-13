cb7e7064756b6f6dd27c7694d05f338358f9c1c9
Markus Holopainen <markusp_holopainen@yahoo.com>
2023-01-13 14:35:32 +0200

    10.15

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View } from 'react-native'
+import { useNavigate } from 'react-router-native'
 import { Formik } from 'formik'
 import * as yup from 'yup'
 import useSignIn from '../hooks/useSignIn'
@@ -28,12 +29,14 @@ const initialValues = { username: '', password: '' }
 
 const SignIn = () => {
   const [signIn] = useSignIn()
+  const navigate = useNavigate()
 
   const onSubmit = async (values) => {
     const { username, password } = values
     try {
       const { data } = await signIn({ username, password })
       console.log(data)
+      navigate('/')
     } catch (e) {
       console.log(e)
     }
--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,14 +1,20 @@
-import { useMutation } from '@apollo/client'
+import { useMutation, useApolloClient } from '@apollo/client'
+import useAuthStorage from '../hooks/useAuthStorage'
 import { AUTHENTICATE } from '../graphql/mutations'
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage()
   const [mutate, result] = useMutation(AUTHENTICATE)
+  const apolloClient = useApolloClient()
 
   const signIn = async ({ username, password }) => {
-    console.log('attempting to sign in with', username, password)
-    return await mutate({
+    const res = await mutate({
       variables: { credentials: { username, password } },
     })
+    const token = res.data.authenticate.accessToken
+    authStorage.setAccessToken(token)
+    apolloClient.resetStore()
+    return res
   }
 
   return [signIn, result]
