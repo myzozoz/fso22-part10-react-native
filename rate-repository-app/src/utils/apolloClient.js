dd8ef5da972886645b9c3fafc382afbdf1dbb677
Markus Holopainen <markusp_holopainen@yahoo.com>
2023-01-13 12:28:46 +0200

    10.12

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import Constants from 'expo-constants'
 
 const httpLink = createHttpLink({
-  // Replace the IP address part with your own IP address!
-  uri: 'http://10.112.18.79:4000/graphql',
+  uri: Constants.manifest.extra.apolloURI,
 })
 
 const createApolloClient = () => {
